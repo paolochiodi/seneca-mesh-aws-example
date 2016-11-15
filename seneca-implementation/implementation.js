7b9a6df8368eb533eb80e9f3c8066f659afd9405
Paolo Chiodi <chiodi84@gmail.com>
2016-11-15 17:20:18 +0100

    extract implementation from base

---
+++ seneca-implementation/implementation.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Seneca = require('seneca')
+
+const seneca = Seneca()
+
+.add({cmd: 'test', action: 'hello'}, function (msg, done) {
+  done(null, {answer: 'world!'})
+})
+
+seneca.use('mesh-aws', {
+  aws: {region: 'eu-central-1'}
+})
+
+
+seneca.ready(function () {
+  console.log('Seneca up and running')
+})
